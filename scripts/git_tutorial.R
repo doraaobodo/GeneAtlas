3afff97855b3920097de663fd4b21435c09dbea0
doraaobodo <46463839+doraaobodo@users.noreply.github.com>
2019-02-12 15:07:22 -0600

    change color of bars

--- scripts/git_tutorial.R
+++ scripts/git_tutorial.R
@@ -5,6 +5,6 @@ resultDir = 'results'
 x = rep(1:6, times = 1:6)
 
 data = data.table(x)
-g = ggplot(data, aes(x = x)) + geom_bar()
+g = ggplot(data, aes(x = x)) + geom_bar(fill = 'yellow')
 g
 ggsave(file.path(resultDir,'geom_bar_plot.png'), g )
