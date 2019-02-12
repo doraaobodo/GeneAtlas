3dabeaa31bc0d4ad1ee42d203e82e419664ddb36
doraaobodo <46463839+doraaobodo@users.noreply.github.com>
2019-02-12 14:31:29 -0600

    fix bug for bar plot

--- scripts/git_tutorial.R
+++ scripts/git_tutorial.R
@@ -2,9 +2,9 @@ library(data.table)
 library(ggplot2)
 
 resultDir = 'results'
-x = 1:6
-y = 1:6
-xy = data.table(x,y)
-g = ggplot(xy, aes(x = x)) + geom_bar()
+x = rep(1:6, times = 1:6)
+
+data = data.table(x)
+g = ggplot(data, aes(x = x)) + geom_bar()
 g
 ggsave(file.path(resultDir,'geom_bar_plot.png'), g )
